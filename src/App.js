fbe58eb7d0c6c449ed2b1700a8bd916c052a568d
Jamal <faizanjamal2018@u.northwestern.edu>
2018-11-14 16:52:58 -0600

    added event handler

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    people: [
+      {name: "Max", age: 28},
+      {name: "Adam", age: 40},
+      {name: "Jess", age: 18}
+    ],
+    otherState: "other value"
+  }
+
+  switchInfoHandler = () => {
+    this.setState({
+      people: [
+        {name: "Joe", age: 28},
+        {name: "Mike", age: 40},
+        {name: "Stephanie", age: 18}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Testing react app</h1>
-        <Person name="Max" age="28" />
-        <Person name="Adam" age="40">Props children</Person>
-        <Person name="Jess" age="18"/>
+        <button onClick={this.switchInfoHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>Props children</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Hi, I\'m an element'));
