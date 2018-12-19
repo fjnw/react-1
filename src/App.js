f4e6f3ef8d1a554bf0a12330963e248e9d2851c4
Jamal <faizanjamal2018@u.northwestern.edu>
2018-12-18 18:41:39 -0600

    input field added with onChange handler binding to props. css added to component.

--- src/App.js
+++ src/App.js
@@ -17,24 +17,46 @@ class App extends Component {
     this.setState({
       people: [
         {name: newName, age: 28},
-        {name: "Mike", age: 40},
-        {name: "Stephanie", age: 18}
+        {name: "Adam", age: 40},
+        {name: "Jess", age: 18}
+      ]
+    })
+  }
+
+  nameChangedHandler  = (event) => {
+    this.setState({
+      people: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 40},
+        {name: "Jess", age: 18}
       ]
     })
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Testing react app</h1>
-        <button onClick={() => this.switchInfoHandler("Lisa")}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchInfoHandler("Lisa")}>Switch Name</button>
         <Person 
           name={this.state.people[0].name}
           age={this.state.people[0].age} />
         <Person 
           name={this.state.people[1].name}
           age={this.state.people[1].age}
-          click={this.switchInfoHandler.bind(this, "Heather")}
+          click={this.switchInfoHandler.bind(this, "Joe")}
+          changed={this.nameChangedHandler}
           >Props children</Person>
         <Person 
            name={this.state.people[2].name}
