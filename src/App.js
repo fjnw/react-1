036d3744aaeba4e22a0b47161f5bcf31934efd37
Jamal <faizanjamal2018@u.northwestern.edu>
2018-12-21 20:43:34 -0600

    5.66 using radiums to use css pseudo selectors.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
+
 
 class App extends Component {
 
@@ -51,7 +53,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -71,6 +77,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     // let classes = ['red', 'bold'].join(' ');
@@ -97,4 +107,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
