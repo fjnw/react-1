e4ffef737d52f346cface5cf12b05afb48c0ffbb
Jamal <faizanjamal2018@u.northwestern.edu>
2018-12-29 15:16:42 -0600

    7.84 converted all components into classes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
-const person  = (props) => {
+class Person extends Component  {
 
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I am {props.name} and am {props.age} years old</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  )
-};
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I am {this.props.name} and am {this.props.age} years old</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    )
+  }
+}
 
-export default person; 
+export default Person; 
