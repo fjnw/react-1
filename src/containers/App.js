8bc05f0dbe7687f8cccb57ad077eadd63503b144
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-05 17:22:14 -0600

    7.90 PureComponents instead of shouldComponentUpdate() checks.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
 
   constructor(props) {
     super(props);
@@ -27,10 +27,12 @@ class App extends Component {
     console.log('[App.js] inside componentDidMount()')
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
-    console.log('[UPDATE] App.js] inside shouldComponentUpdate()', nextProps, nextState);
-    return true;
-  }
+  // shouldComponentUpdate (nextProps, nextState) {
+  //   console.log('[UPDATE] App.js] inside shouldComponentUpdate()', nextProps, nextState);
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;
+  //   // return true;
+  // }
 
   componentWillUpdate (nextProps, nextState) {
     console.log('[UPDATE App.js inside componentWillUpdate()', nextProps, nextState)
@@ -85,6 +87,7 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
+        <button onClick={() => {this.setState({showPersons:true})}}>Show Persons</button>
           <Cockpit 
             appTitle={this.props.title}
             showPersons={this.state.showPersons}
