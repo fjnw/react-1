19a1a5018e9f5f775fa49b08d6c0f026c35e42ce
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-05 20:23:44 -0600

    7.98 changed WithClass to withClass and now contains a non-functional component that returns a functional wrapped component and used in App.js... contains error that props aren't passed to the wrapped component to display name and age.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
-import WithClass from '../../../hoc/WithClass';
+import withClass from '../../../hoc/withClass';
 
 
 class Person extends Component  {
@@ -31,11 +31,11 @@ class Person extends Component  {
     console.log(['[Person.js] inside render()'])
 
     return (
-      <WithClass classes={classes.Person}>
+      <>
         <p onClick={this.props.click}>I am {this.props.name} and am {this.props.age} years old</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </WithClass>
+      </>
     )
     // return [
     //   <p key="1" onClick={this.props.click}>I am {this.props.name} and am {this.props.age} years old</p>,
@@ -45,4 +45,4 @@ class Person extends Component  {
   }
 }
 
-export default Person; 
+export default withClass(Person, classes.Person); 
